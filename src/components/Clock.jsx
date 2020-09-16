55e5d19fd92399b63c52b02c2c09be31494c991a
nibedan-pal <69342637+nibedan-pal@users.noreply.github.com>
2020-09-16 09:11:13 +0530

    Update Clock.jsx

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,34 +1,60 @@
-import React from 'react';
-
+import React from "react";
+import "./Clock.css";
 class Clock extends React.Component {
-    formatTime(timeInSeconds) {
-        var seconds = timeInSeconds % 60;
-        var minutes = Math.floor(timeInSeconds / 60);
+	constructor(props) {
+		super(props);
+		this.state = {
+			timeInSec: 0,
+		};
+	}
+	formatTime(timeInSeconds) {
+		var seconds = timeInSeconds % 60;
+		var minutes = Math.floor(timeInSeconds / 60);
 
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
+		if (seconds < 10) {
+			seconds = "0" + seconds;
+		}
 
-        if (minutes < 10) {
-            minutes = '0' + minutes;
-        }
+		if (minutes < 10) {
+			minutes = "0" + minutes;
+		}
 
-        return minutes + ':' + seconds;
-    }
+		return minutes + ":" + seconds;
+	}
 
-    render() {
-        var {timeInSeconds} = this.props;
-        //Keep the classes name. Try to inject your code and do not remove existing code
-        return (
-            <div className="clock">
-                <span className="clock-text">
-                  
-                </span>
-            </div>
-        );
-    }
+	startTimer = () => {
+		const { timeInSeconds } = this.props;
+		this.clock = setInterval(() => {
+			this.setState({
+				timeInSec: this.state.timeInSec - 1,
+			});
+		}, 1000);
+
+		setTimeout(() => {
+			clearInterval(this.clock);
+		}, timeInSeconds * 1000);
+	};
+
+	componentDidUpdate(prevProps) {
+		if (
+			this.props.timeInSeconds !== prevProps.timeInSeconds &&
+			this.state.timeInSec === 0
+		) {
+			this.setState({ timeInSec: this.props.timeInSeconds });
+			this.startTimer();
+		}
+	}
+
+	render() {
+		const { timeInSec } = this.state;
+		return (
+			<div className="clock clock-container">
+				<span className="clock-text clock-value">
+					{this.formatTime(timeInSec)}
+				</span>
+			</div>
+		);
+	}
 }
-
-
 
 export default Clock;
