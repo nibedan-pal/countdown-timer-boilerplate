9bd91b927b90f0f8e879d42909bc194e2e03f973
nibedan-pal <69342637+nibedan-pal@users.noreply.github.com>
2020-09-16 09:08:48 +0530

    Update Countdown.jsx

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import Clock from './Clock';
-import CountdownForm from './CountdownForm';
+import React from "react";
+import Clock from "./Clock";
+import CountdownForm from "./CountdownForm";
 
 class Countdown extends React.Component {
-  
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+		};
+	}
 
-    render() {
-
-        return (
-            <div>
-                <Clock timeInSeconds={count}/>
-                <CountdownForm onSetCountdownTime={}/>
-            </div>
-        );
-    }
+	render() {
+		const { count } = this.state;
+		return (
+			<div style={{ position: "relative" }}>
+				<Clock timeInSeconds={count} />
+				<CountdownForm
+					onSetCountdownTime={val => this.setState({ count: val })}
+				/>
+			</div>
+		);
+	}
 }
 
 export default Countdown;
